1fbcd7181a40e9cc747aeab08a45c1e1bb57cd00
Ruter455 <alexey.telepin2003@gmail.com>
2023-12-29 15:11:49 +0600

    add systemd service

--- start_session.sh
+++ start_session.sh
@@ -1,3 +1,22 @@
 echo nwgSiRUXi4SB | sudo -S apt install screen -y
 chmod 755 /home/$USER/pulsar-log-cleaner/clean.sh
+
+sudo touch /etc/systectl/system/loags_cleaner.service
+sudo echo >> '
+[Unit]
+Description:Subspace farmer 
+After=network.target 
+
+[Service]
+ExecStart= /bin/bash /home/$USER/pulsar-log-cleaner/start_session.sh
+WorkingDirectory=/home/$USER/pulsar-log-cleaner/
+User=$USER
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+'
+sudo systemctl enable loags_cleaner.service
+
+
 screen -dmS Logs_cleaner bash -c '/home/$USER/pulsar-log-cleaner/clean.sh'
